b576c699178b00efcabb9746a9c40c15205d2d2b
mac <mac@macs-MacBook-Pro.local>
2020-08-21 01:14:53 +0700

    u

--- src/Core/Orders/Services/OrderService.php
+++ src/Core/Orders/Services/OrderService.php
@@ -848,9 +848,9 @@ class OrderService extends BaseService implements OrderServiceInterface
             $discount->total = $total;
         }
 
-        // $pdf = PDF::loadView('hub::pdf.order-invoice', $data);
+        $pdf = PDF::loadView(config('getcandy.invoicing.pdf', 'hub::pdf.order-invoice'), $data);
 
-        return "";
+        return $pdf;
     }
 
     /**
