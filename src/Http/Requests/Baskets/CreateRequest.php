1be2324d0d35ce129a7fb6a1345c8a2ae29c6f54
Ibid Athoillah <ibidathoillah@gmail.com>
2020-11-10 07:52:22 +0700

    update

--- src/Http/Requests/Baskets/CreateRequest.php
+++ src/Http/Requests/Baskets/CreateRequest.php
@@ -17,6 +17,7 @@ class CreateRequest extends FormRequest
         return true;
     }
 
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -25,8 +26,8 @@ class CreateRequest extends FormRequest
     public function rules()
     {
         $rules = [
+            'basket_id' => 'sometimes|hashid_is_valid:baskets',
             'variants' => 'array|unique_lines',
-            'basket_id' => 'hashid_is_valid:baskets',
         ];
 
         $variants = app('api')->productVariants()->getByHashedIds(
@@ -38,7 +39,8 @@ class CreateRequest extends FormRequest
                 return $variant->encodedId() === $v['id'] ?? null;
             });
             if ($variant) {
-                $rules["variants.{$i}.quantity"] = 'required|numeric|min:1|min_quantity:'.$variant->min_qty.'|min_batch:'.$variant->min_batch.'|in_stock:'.$v['id'] ?? '0';
+                $max = $variant->max_qty>0 ? $variant->max_qty : 999999 ;
+                $rules["variants.{$i}.quantity"] = 'required|numeric|min:1|min_quantity:'.$variant->min_qty.'|max:'.$max.'|min_batch:'.$variant->min_batch.'|in_stock:'.$v['id'] ?? '0';
             }
             $rules["variants.{$i}.id"] = 'required|hashid_is_valid:product_variants';
         }
@@ -51,6 +53,7 @@ class CreateRequest extends FormRequest
         return [
             'variants.*.id.hashid_is_valid' => trans('getcandy::validation.hashid_is_valid'),
             'variants.*.quantity.min_quantity' => trans('getcandy::validation.min_qty'),
+            'variants.*.quantity.max' => trans('getcandy::validation.max_qty'),
             'variants.*.quantity.min_batch' => trans('getcandy::validation.min_batch'),
         ];
     }
