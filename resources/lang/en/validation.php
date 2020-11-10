6713c31bcfbb2d45a963b5a2e4b573a71eef8755
Ibid Athoillah <ibidathoillah@gmail.com>
2020-11-10 07:58:52 +0700

    update

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -3,7 +3,7 @@
 return [
     'hashid_is_valid' => 'The variant ID is incorrect',
     'in_stock' => 'Melebihi stock yang tesedia',
-    'max_qty' => 'Anda melebihi maksimal pembelian :max_qty',
+    'max_qty' => 'Anda melebihi maksimal pembelian :max',
     'min_qty' => 'Kamu paling tidak bisa membeli minimal :min_qty items',
     'min_batch' => 'Kamu paling tidak bisa membeli kelipatan dari :min_batch',
 ];
